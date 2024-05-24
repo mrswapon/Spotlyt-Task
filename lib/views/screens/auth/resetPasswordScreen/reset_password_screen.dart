fd99fd615e340d3598d38318c8848a32cc6291b6
mrswapon <mrswapon15@gmail.com>
2024-05-24 15:38:57 +0600

    some isssue change

--- lib/views/screens/auth/resetPasswordScreen/reset_password_screen.dart
+++ lib/views/screens/auth/resetPasswordScreen/reset_password_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
 import '../../../../utils/app_images.dart';
 import '../../../../utils/app_strings.dart';
 import '../../../widgets/custom_text.dart';
